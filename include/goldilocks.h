a53bc54e22fe8031d263b255b9c7d5027b602e87
psyduck <zhangzh0410@gmal.com>
2025-04-18 15:19:10 +0800

    fixes

--- include/goldilocks.h
+++ include/goldilocks.h
@@ -2,36 +2,38 @@
 #define _GOLDILOCKS_H_
 
 #include <cstdint>
-typedef uint64_t i64;
-typedef __uint128_t i128;
+#define GOLDILOCKS_ORDER ((1ull << 32) - 1) << 32 + 1
+
 class goldilocks{
+    typedef uint64_t i64;
+    typedef __uint128_t i128;
     private:
         i64 p = (((1ull << 32) - 1) << 32) + 1;
     public:
         i64 val;
-        goldilocks(i64 v);
-        goldilocks():val(0){}
-        bool operator==(const goldilocks& n) const;
+        goldilocks(i64 v = 0);
+        // goldilocks();
         goldilocks  operator+(const goldilocks& n) const;
         goldilocks  operator-(const goldilocks& n) const;
         goldilocks  operator*(const goldilocks& n) const;
-        goldilocks  operator=(const i64 n);
-        goldilocks  operator=(const goldilocks& n);
+        goldilocks&  operator=(const i64 n);
+        goldilocks&  operator=(const goldilocks& n);
+        bool operator==(const goldilocks&n) const;
         void print() const;
 };
 
 class goldilocksFp2{
+    typedef uint64_t i64;
+    typedef __uint128_t i128;
     private:
         static goldilocks w; // ω^2 = w
     public:
         goldilocks a, b;
         goldilocksFp2(i64 a, i64 b);
         goldilocksFp2(goldilocks a, goldilocks b);
-        bool operator==(const goldilocksFp2& n) const;
-        goldilocksFp2 operator+(const goldilocksFp2& n) const;
-        goldilocksFp2 operator-(const goldilocksFp2& n) const;
-        goldilocksFp2 operator*(const goldilocksFp2& n) const;
-        goldilocksFp2 operator=(const goldilocksFp2& n);
+        goldilocksFp2  operator+(const goldilocksFp2& n) const;
+        goldilocksFp2  operator-(const goldilocksFp2& n) const;
+        goldilocksFp2  operator*(const goldilocksFp2& n) const;
         void print() const;
 };
 
--- src/goldilocks.cpp
+++ src/goldilocks.cpp
@@ -6,12 +6,10 @@
 typedef uint64_t i64;
 typedef __uint128_t i128;
 
-goldilocks::goldilocks(i64 v = 0){
+goldilocks::goldilocks(i64 v){
     val = v % p;
 }
-bool goldilocks::operator==(const goldilocks& n) const{
-    return val == n.val;
-}
+// goldilocks::goldilocks() : val(0) {}
 goldilocks goldilocks::operator+(const goldilocks& n) const{
     i128 sum = (i128)val + n.val;
     return goldilocks((i64)(sum % p));
@@ -24,12 +22,15 @@ goldilocks goldilocks::operator*(const goldilocks& n) const{
     i128 product = (i128)val * n.val;
     return goldilocks((i64)(product % p));
 }
-goldilocks goldilocks::operator=(const i64 n){
+bool goldilocks::operator==(const goldilocks& n) const{
+    return val == n.val;
+}
+goldilocks& goldilocks::operator=(const i64 n){
     val = n % p;
     return *this;
 }
-goldilocks goldilocks::operator=(const goldilocks& n){
-    if (this != &n){
+goldilocks& goldilocks::operator=(const goldilocks& n){
+    if(this != &n){
         val = n.val;
     }
     return *this;
@@ -41,9 +42,6 @@ void goldilocks::print() const {
 goldilocks goldilocksFp2::w = 7;
 goldilocksFp2::goldilocksFp2(i64 a, i64 b):a(goldilocks(a)),b(goldilocks(b)){}
 goldilocksFp2::goldilocksFp2(goldilocks a, goldilocks b):a(a),b(b){}
-bool goldilocksFp2::operator==(const goldilocksFp2& n) const{
-    return a == n.a && b == n.b;
-}
 goldilocksFp2 goldilocksFp2::operator+(const goldilocksFp2& n) const{
     return goldilocksFp2(a + n.a, b + n.b);
 }
@@ -56,13 +54,6 @@ goldilocksFp2 goldilocksFp2::operator*(const goldilocksFp2& n) const{
     goldilocks ad = a * n.b;
     goldilocks bc = b * n.a;
     return goldilocksFp2(ac + bd * w, ad + bc);
-}
-goldilocksFp2 goldilocksFp2::operator=(const goldilocksFp2& n){
-    if(this != &n){
-        a = n.a;
-        b = n.b;
-    }
-    return *this;
 }
 void goldilocksFp2::print() const{
     std::cout << a.val << " + " << b.val << "ω" << std::endl;
